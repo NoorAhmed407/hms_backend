b32cc81f0f3ebeb9f9496b5a477c1dcef21c23e8
noorahmed407 <nq73399@gmail.com>
2021-10-30 21:41:17 +0500

    Updated appointment Model

---
+++ models/Appointment.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//Create Ninja Schema and model
+const AppointmentSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Name field is required"],
+  },
+
+  doctor_id: {
+    type: Number,
+    required: [true, "doctor_id field is required"],
+  },
+
+  date_time: {
+    type: Date,
+    required: [true, "date_time field is required"],
+  },
+  status: {
+    type: String,
+    default: "pending",
+  },
+});
+
+const Appointment = mongoose.model("appointment", AppointmentSchema);
+
+module.exports = Appointment;
